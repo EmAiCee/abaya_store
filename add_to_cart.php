9b006b312f1ba965098f1b94d95a820c466db742
Mohammed Amir Hassan <mohammedamirhassan2005@gmail.com>
2025-05-27 17:20:40 +0100

    Update add_to_cart logic with session handling

--- add_to_cart.php
+++ add_to_cart.php
@@ -19,20 +19,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('Location: index.php');
     exit();
 }
-?>    $product_id = $_POST['product_id'];
-    $quantity = $_POST['quantity'];
-
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = [];
-    }
-
-    if (isset($_SESSION['cart'][$product_id])) {
-        $_SESSION['cart'][$product_id] += $quantity;
-    } else {
-        $_SESSION['cart'][$product_id] = $quantity;
-    }
-
-    header('Location: index.php');
-    exit();
-}
 ?>
