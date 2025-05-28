d1d055893a7fd4405c0c798dc7f744d38fd780ad
TamamDot <Ohaneey@gmail.com>
2025-05-28 04:21:51 +0100

    Worked on the backend process for the checkout page

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,25 +1,18 @@
-
-<?php
-session_start();
-include 'db_connect.php';
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $product_id = $_POST['product_id'];
-    $quantity = $_POST['quantity'];
-
-    if (!isset($_SESSION['cart'])) {
-        $_SESSION['cart'] = [];
+<?php 
+    session_start();
+    include 'db_connect.php';
+    if ($_POST["prod_array"]) {
+        $prodArray = explode(",",$_POST["prod_array"]);
+        $products = array();
+        // echo $prodArray[0];
+        for ($h = 0; $h < count($prodArray); $h++) {
+            $prod_values = explode(" ",$prodArray[$h]);
+            echo $prod_values[0].",".$prod_values[1];
+            
+            $products["$prod_values[0]"] = "$prod_values[1]";
+        }
+        $_SESSION['cart'] = $products;
+        header("Location:checkout.php");
+        exit();
     }
-
-    if (isset($_SESSION['cart'][$product_id])) {
-        $_SESSION['cart'][$product_id] += $quantity;
-    } else {
-        $_SESSION['cart'][$product_id] = $quantity;
-    }
-
-    header('Location: index.php');
-    exit();
-}
 ?>
-
-
